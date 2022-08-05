b3385fb8b761f21a5d0ae23f77f09424a6770fc5
haseebdivinatorysolutions <haseeb@divinatorysolutions.com>
2022-08-05 16:40:56 +0500

    Second Commit

---
+++ vue/src/App.vue
@@ -0,0 +1,19 @@
+<script >
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default{
+    components: { HelloWorld }
+}
+</script>
+
+<template>
+ 
+<HelloWorld></HelloWorld>
+</template>
+
+<style scoped>
+</style>
---
+++ vue/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+    state () {
+      return {
+        count: 0
+      }
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      }
+    },
+    action:{},
+    getters:{},
+
+
+  })
+
+  export default store;
+
+
+ 
+  
